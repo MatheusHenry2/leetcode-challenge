d6a03b88ad956933794b6834a3a2660786d33285
MatheusHenry2 <matheus.play7@live.com>
2025-02-26 21:27:40 -0300

    Palindomer number solution with for and string

---
+++ PalindromeNumber.java
@@ -0,0 +1,16 @@
+public class PalindromeNumber {
+
+    public static boolean isPalindrome(int x) {
+        String normalString = String.valueOf(x);
+        String reverseString = "";
+        for (int i = normalString.length() - 1; i >= 0; i--){
+            reverseString = reverseString + normalString.charAt(i);
+        }
+        return (normalString.equals(reverseString));
+    }
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        System.out.println(isPalindrome(10));
+    }
+}
