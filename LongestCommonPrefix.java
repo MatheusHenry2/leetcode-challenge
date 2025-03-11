a6b085eaca32d9f83ea7751a68de6efb7c8ccefa
MatheusHenry2 <matheus.play7@live.com>
2025-03-10 21:56:02 -0300

    longest prefix leetcode

---
+++ LongestCommonPrefix.java
@@ -0,0 +1,26 @@
+public class LongestCommonPrefix {
+
+    public static String longestCommonPrefix(String[] strs) {
+        String longestPrefix = strs[0];
+        int sizeOfEachWord;
+        for (int i = 1; i < strs.length; i++) {
+            sizeOfEachWord = strs[i].length();
+            for(int j=0; j < longestPrefix.length(); j++) {
+                if(j >= sizeOfEachWord || longestPrefix.charAt(j) != strs[i].charAt(j)) {
+                    longestPrefix = longestPrefix.substring(0, j);
+                    break;
+                }
+            }
+            if(longestPrefix.equals(" ")) {
+                return " ";
+            }
+        }
+        return longestPrefix;
+    }
+
+    public static void main(String[] args) {
+        String[] strs = {"flower","flow","flight"};
+        System.out.println(longestCommonPrefix(strs));
+
+    }
+}
