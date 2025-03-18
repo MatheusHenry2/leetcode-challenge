e109328614319bc660e6ab419631930344a70160
MatheusHenry2 <matheus.play7@live.com>
2025-03-17 22:19:45 -0300

    Power of two

---
+++ PowerOfTwo.java
@@ -0,0 +1,14 @@
+public class PowerOfTwo {
+
+    public static boolean isPowerOfTwo(int n) {
+        for (long i = 0; i < n; i++) {
+            if ((long) Math.pow(2, i) == n) return true;
+            else if ((long) Math.pow(2, i) > n) break;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPowerOfTwo(4));
+    }
+}
